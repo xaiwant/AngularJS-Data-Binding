2b1600b4540ef64e1084a569a5b8d94ee9985887
Jaywant Topno <jaywanttopno@yahoo.com>
2018-01-31 20:39:33 +0530

    Initial commit to push codebase.

---
+++ DataBinding/angular-binding.js
@@ -0,0 +1,7 @@
+
+		  var myApp = angular.module('myApp', []);
+              myApp.controller('myController', function($scope) {
+                 $scope.name = 'James Bond';
+				 $scope.user = 'Jaywant Topno';
+				 $scope.person = 'Anonymous'
+          });
